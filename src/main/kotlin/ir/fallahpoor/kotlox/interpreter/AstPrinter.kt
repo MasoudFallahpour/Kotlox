6793c39f6223249c628093e191dbd50c318f5423
Masood Fallahpoor <masood.fallahpoor@gmail.com>
2021-11-22 15:50:51 +0100

    Add AstPrinter.kt for debugging purposes

---
+++ src/main/kotlin/ir/fallahpoor/kotlox/interpreter/AstPrinter.kt
@@ -0,0 +1,38 @@
+package ir.fallahpoor.kotlox.interpreter
+
+class AstPrinter : Expr.Visitor<String> {
+
+    fun print(expr: Expr): String = expr.accept(this)
+
+    override fun visitBinaryExpr(expr: Expr.Binary): String =
+        parenthesize(
+            expr.operator.lexeme,
+            expr.left,
+            expr.right
+        )
+
+    override fun visitGroupingExpr(expr: Expr.Grouping): String =
+        parenthesize("group", expr.expression)
+
+    override fun visitLiteralExpr(expr: Expr.Literal): String =
+        if (expr.value == null) {
+            "nil"
+        } else {
+            expr.value.toString()
+        }
+
+    override fun visitUnaryExpr(expr: Expr.Unary): String =
+        parenthesize(expr.operator.lexeme, expr.right)
+
+    private fun parenthesize(name: String, vararg exprs: Expr): String {
+        val builder = StringBuilder()
+        builder.append("(").append(name)
+        for (expr in exprs) {
+            builder.append(" ")
+            builder.append(expr.accept(this))
+        }
+        builder.append(")")
+        return builder.toString()
+    }
+
+}
