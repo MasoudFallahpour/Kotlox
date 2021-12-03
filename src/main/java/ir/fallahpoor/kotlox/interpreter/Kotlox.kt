deb96098dceb6dedc1115d312056ab4725f90d8e
Masood Fallahpoor <masood.fallahpoor@gmail.com>
2021-12-04 00:07:20 +0100

    Refactor Kotlox

--- src/main/java/ir/fallahpoor/kotlox/interpreter/Kotlox.kt
+++ src/main/java/ir/fallahpoor/kotlox/interpreter/Kotlox.kt
@@ -5,27 +5,24 @@ import ir.fallahpoor.kotlox.interpreter.parser.Parser
 import ir.fallahpoor.kotlox.interpreter.parser.Tokens
 import ir.fallahpoor.kotlox.interpreter.scanner.Scanner
 import ir.fallahpoor.kotlox.interpreter.scanner.Token
-import java.io.BufferedReader
 import java.io.IOException
-import java.io.InputStreamReader
 import java.nio.file.Files
 import java.nio.file.Paths
 import kotlin.system.exitProcess
 
 class Lox(private val commandLineArgs: Array<String>) {
 
-    private val printer = Printer()
-    private val errorReporter = ErrorReporter(printer)
-    private val interpreter = Interpreter(errorReporter, printer)
+    private val errorReporter = ErrorReporter(Printer())
+    private val interpreter = Interpreter(errorReporter, Printer())
 
     private object ErrorCode {
-        // Lox is used with an incorrect number of arguments
+        // Wrong number of arguments when invoking Kotlox
         const val WRONG_USAGE = 64
 
-        // There is a syntax error in some way
+        // Syntax error
         const val WRONG_SYNTAX = 65
 
-        // There an internal error like adding a number to a string
+        // Internal error like adding a number to a string
         const val RUNTIME_ERROR = 70
     }
 
@@ -34,7 +31,7 @@ class Lox(private val commandLineArgs: Array<String>) {
             0 -> runPrompt()
             1 -> runFile(commandLineArgs[0])
             else -> {
-                println("Usage: Kotlox [script]")
+                println("Usage: Kotlox [script_file]")
                 exitProcess(ErrorCode.WRONG_USAGE)
             }
         }
@@ -42,13 +39,12 @@ class Lox(private val commandLineArgs: Array<String>) {
 
     @Throws(IOException::class)
     private fun runPrompt() {
-        val input = InputStreamReader(System.`in`)
-        val reader = BufferedReader(input)
+        val input = java.util.Scanner(System.`in`)
         while (true) {
             print("> ")
             // The only case that readLine returns null is when user presses Ctrl+C to exit the interactive mode.
             // That's the time to break out of the loop.
-            val line = reader.readLine() ?: break
+            val line = input.nextLine() ?: break
             run(line)
             errorReporter.hadError = false
             errorReporter.hadRuntimeError = false
@@ -68,10 +64,7 @@ class Lox(private val commandLineArgs: Array<String>) {
     }
 
     private fun run(source: String) {
-        val scanner = Scanner(source, errorReporter)
-        val tokens: List<Token> = scanner.scanTokens()
-        val parser = Parser(Tokens(tokens), errorReporter)
-        val statements: List<Stmt> = parser.parse()
+        val statements: List<Stmt> = parseSource(source)
         // Stop if there was a syntax error.
         if (errorReporter.hadError) {
             println()
@@ -79,7 +72,8 @@ class Lox(private val commandLineArgs: Array<String>) {
         }
         if (statements.isEmpty()) {
             return
-        } else if (statements.size == 1) {
+        }
+        if (statements.size == 1) {
             val statement = statements[0]
             if (statement is Stmt.Expression) {
                 val printStmt = Stmt.Print(statement.expression)
@@ -93,6 +87,13 @@ class Lox(private val commandLineArgs: Array<String>) {
         if (errorReporter.hadRuntimeError) {
             println()
         }
+    }
+
+    private fun parseSource(source: String): List<Stmt> {
+        val scanner = Scanner(source, errorReporter)
+        val tokens: List<Token> = scanner.scanTokens()
+        val parser = Parser(Tokens(tokens), errorReporter)
+        return parser.parse()
     }
 
 }
