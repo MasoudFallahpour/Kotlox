7da10a74dc92eef170cf602158dea3fe9880713d
Masood Fallahpoor <masood.fallahpoor@gmail.com>
2023-05-23 13:53:47 +0200

    Refactor GenerateAst.kt

--- src/main/java/ir/fallahpoor/kotlox/tools/GenerateAst.kt
+++ src/main/java/ir/fallahpoor/kotlox/tools/GenerateAst.kt
@@ -5,55 +5,59 @@ import java.io.PrintWriter
 private const val SINGLE_INDENT = "    "
 private const val DOUBLE_INDENT = SINGLE_INDENT + SINGLE_INDENT
 private const val TRIPLE_INDENT = SINGLE_INDENT + DOUBLE_INDENT
+private const val PACKAGE_NAME = "ir.fallahpoor.kotlox.interpreter"
 
+// We use this code to generate the Expr.kt and Stmt.kt automatically without the
+// need to create them manually. So here we're generating Kotlin classes. We could use
+// a library like KotlinPoet to do this, but it's too much for what we want to do.
 fun main() {
     val outputDir = "./src/main/java/ir/fallahpoor/kotlox/interpreter"
-    defineAst(
-        outputDir,
-        "Expr",
-        listOf(
-            "Assign   -> name: Token, value: Expr",
-            "Binary   -> left: Expr, operator: Token, right: Expr",
-            "Call     -> callee: Expr, paren: Token, arguments: List<Expr>",
-            "Grouping -> expression: Expr",
-            "Literal  -> value: Any?",
-            "Logical  -> left: Expr, operator: Token, right: Expr",
-            "Unary    -> operator: Token, right: Expr",
-            "Variable -> name: Token"
-        )
+    defineAbstractSyntaxTree(
+            outputDir = outputDir,
+            className = "Expr",
+            subclasses = listOf(
+                    "Assign   -> name: Token, value: Expr",
+                    "Binary   -> left: Expr, operator: Token, right: Expr",
+                    "Call     -> callee: Expr, paren: Token, arguments: List<Expr>",
+                    "Grouping -> expression: Expr",
+                    "Literal  -> value: Any?",
+                    "Logical  -> left: Expr, operator: Token, right: Expr",
+                    "Unary    -> operator: Token, right: Expr",
+                    "Variable -> name: Token"
+            )
     )
-    defineAst(
-        outputDir,
-        "Stmt",
-        listOf(
-            "Block      -> statements: List<Stmt>",
-            "Expression -> expression: Expr",
-            "Function   -> name: Token, params: List<Token>, body: List<Stmt>",
-            "If         -> condition: Expr, thenBranch: Stmt, elseBranch: Stmt?",
-            "Print      -> expression: Expr",
-            "Return     -> keyword: Token, value: Expr?",
-            "Var        -> name: Token, initializer: Expr?",
-            "While      -> condition: Expr, body: Stmt",
-            "Break      ->"
-        )
+    defineAbstractSyntaxTree(
+            outputDir = outputDir,
+            className = "Stmt",
+            subclasses = listOf(
+                    "Block      -> statements: List<Stmt>",
+                    "Expression -> expression: Expr",
+                    "Function   -> name: Token, params: List<Token>, body: List<Stmt>",
+                    "If         -> condition: Expr, thenBranch: Stmt, elseBranch: Stmt?",
+                    "Print      -> expression: Expr",
+                    "Return     -> keyword: Token, value: Expr?",
+                    "Var        -> name: Token, initializer: Expr?",
+                    "While      -> condition: Expr, body: Stmt",
+                    "Break      ->"
+            )
     )
 }
 
-private fun defineAst(outputDir: String, baseName: String, types: List<String>) {
-    val path = "$outputDir/$baseName.kt"
+private fun defineAbstractSyntaxTree(outputDir: String, className: String, subclasses: List<String>) {
+    val path = "$outputDir/$className.kt"
     val writer = PrintWriter(path, "UTF-8")
-    writer.println("package ir.fallahpoor.kotlox.interpreter")
+    writer.println("package $PACKAGE_NAME")
     writer.println()
-    writer.println("import ir.fallahpoor.kotlox.interpreter.scanner.Token")
+    writer.println("import $PACKAGE_NAME.scanner.Token")
     writer.println()
-    writer.println("sealed class $baseName {")
+    writer.println("sealed class $className {")
     writer.println()
-    defineVisitor(writer, baseName, types)
+    defineVisitorInterface(writer = writer, className = className, types = subclasses)
     writer.println()
-    for (type in types) {
-        val className = type.split("->")[0].trim()
+    for (type in subclasses) {
+        val subclassName = type.split("->")[0].trim()
         val propertyList = type.split("->")[1].trim()
-        defineType(writer, baseName, className, propertyList)
+        defineSubclass(writer = writer, className = className, subclassName = subclassName, propertyList = propertyList)
     }
     writer.println("${SINGLE_INDENT}abstract fun <R> accept(visitor: Visitor<R>): R")
     writer.println()
@@ -61,15 +65,15 @@ private fun defineAst(outputDir: String, baseName: String, types: List<String>) 
     writer.close()
 }
 
-private fun defineType(
-    writer: PrintWriter,
-    baseName: String,
-    className: String,
-    propertyList: String
+private fun defineSubclass(
+        writer: PrintWriter,
+        className: String,
+        subclassName: String,
+        propertyList: String
 ) {
     val properties = propertyList.split(", ").filter { it.isNotBlank() }
     if (properties.isNotEmpty()) {
-        writer.println("${SINGLE_INDENT}data class $className(")
+        writer.println("${SINGLE_INDENT}data class $subclassName(")
         properties.forEachIndexed { i, v ->
             if (i != properties.lastIndex) {
                 writer.println("${DOUBLE_INDENT}val $v,")
@@ -79,23 +83,23 @@ private fun defineType(
         }
         writer.print("${SINGLE_INDENT})")
     } else {
-        writer.print("${SINGLE_INDENT}object $className")
+        writer.print("${SINGLE_INDENT}object $subclassName")
     }
-    writer.println(" : $baseName() {")
+    writer.println(" : $className() {")
     writer.println("${DOUBLE_INDENT}override fun <R> accept(visitor: Visitor<R>): R {")
-    writer.println("${TRIPLE_INDENT}return visitor.visit$className$baseName(this)")
-    writer.println("${DOUBLE_INDENT}}")
-    writer.println("${SINGLE_INDENT}}")
+    writer.println("${TRIPLE_INDENT}return visitor.visit$subclassName$className(this)")
+    writer.println("$DOUBLE_INDENT}")
+    writer.println("$SINGLE_INDENT}")
     writer.println()
 }
 
-private fun defineVisitor(writer: PrintWriter, baseName: String, types: List<String>) {
+private fun defineVisitorInterface(writer: PrintWriter, className: String, types: List<String>) {
     writer.println("${SINGLE_INDENT}interface Visitor<R> {")
     for (type: String in types) {
         val typeName = type.split("->")[0].trim()
         writer.println(
-            "${DOUBLE_INDENT}fun visit$typeName$baseName(${baseName.lowercase()}: $typeName): R"
+                "${DOUBLE_INDENT}fun visit$typeName$className(${className.lowercase()}: $typeName): R"
         )
     }
-    writer.println("${SINGLE_INDENT}}")
+    writer.println("$SINGLE_INDENT}")
 }
