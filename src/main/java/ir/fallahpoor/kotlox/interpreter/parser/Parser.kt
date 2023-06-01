74fb292af27a7b6c330ba0c2b74bbd3be2fa977f
Masood Fallahpoor <masood.fallahpoor@gmail.com>
2023-06-01 16:42:29 +0200

    Refactor the Tokens class and add its missing unit tests

--- src/main/java/ir/fallahpoor/kotlox/interpreter/parser/Parser.kt
+++ src/main/java/ir/fallahpoor/kotlox/interpreter/parser/Parser.kt
@@ -12,6 +12,7 @@ import java.util.*
  * This class implements a recursive descent parser for the following grammar. The grammar is
  * unambiguous and has no left-recursive rules. Otherwise, it would not be possible to implement
  * such a parser.
+ *
  * program     -> declaration* EOF
  * declaration -> funcDecl | varDecl | statement
  * funcDecl    -> "fun" function
@@ -41,7 +42,8 @@ import java.util.*
  * primary     -> NUMBER | STRING | IDENTIFIER | "true" | "false" | "nil" | "(" expression ")"
  */
 class Parser(
-    private val tokens: Tokens, private val errorReporter: ErrorReporter
+    private val tokens: Tokens,
+    private val errorReporter: ErrorReporter
 ) {
 
     companion object {
@@ -66,9 +68,9 @@ class Parser(
 
     private fun declaration(): Stmt? {
         return try {
-            if (tokens.getNextTokenIfItHasType((TokenType.FUN))) {
+            if (tokens.consumeNextTokenIfItHasType((TokenType.FUN))) {
                 function("function")
-            } else if (tokens.getNextTokenIfItHasType(TokenType.VAR)) {
+            } else if (tokens.consumeNextTokenIfItHasType(TokenType.VAR)) {
                 varDeclaration()
             } else {
                 statement()
@@ -80,52 +82,52 @@ class Parser(
     }
 
     private fun function(kind: String): Stmt.Function {
-        val name = consumeTokenOrThrowError(TokenType.IDENTIFIER, "Expect $kind name.")
-        consumeTokenOrThrowError(TokenType.LEFT_PAREN, "Expect '(' after $kind name.")
+        val name = consumeNextTokenOrThrowError(TokenType.IDENTIFIER, "Expect $kind name.")
+        consumeNextTokenOrThrowError(TokenType.LEFT_PAREN, "Expect '(' after $kind name.")
         val parameters = mutableListOf<Token>()
         if (!tokens.nextTokenHasType(TokenType.RIGHT_PAREN)) {
             do {
                 if (parameters.size >= FUNCTION_CALL_MAX_ARGUMENT_LIST_SIZE) {
                     errorReporter.error(tokens.peekNextToken(), "Can't have more than 255 parameters.")
                 }
-                parameters += consumeTokenOrThrowError(TokenType.IDENTIFIER, "Expect parameter name.")
-            } while (tokens.getNextTokenIfItHasType(TokenType.COMMA))
+                parameters += consumeNextTokenOrThrowError(TokenType.IDENTIFIER, "Expect parameter name.")
+            } while (tokens.consumeNextTokenIfItHasType(TokenType.COMMA))
         }
-        consumeTokenOrThrowError(TokenType.RIGHT_PAREN, "Expect ')' after parameters.")
-        consumeTokenOrThrowError(TokenType.LEFT_BRACE, "Expect '{' before $kind body.")
+        consumeNextTokenOrThrowError(TokenType.RIGHT_PAREN, "Expect ')' after parameters.")
+        consumeNextTokenOrThrowError(TokenType.LEFT_BRACE, "Expect '{' before $kind body.")
         val body = block()
         return Stmt.Function(name, parameters, body)
     }
 
     private fun varDeclaration(): Stmt {
-        val name: Token = consumeTokenOrThrowError(TokenType.IDENTIFIER, "Expect variable name.")
+        val name: Token = consumeNextTokenOrThrowError(TokenType.IDENTIFIER, "Expect variable name.")
         var initializer: Expr? = null
-        if (tokens.getNextTokenIfItHasType(TokenType.EQUAL)) {
+        if (tokens.consumeNextTokenIfItHasType(TokenType.EQUAL)) {
             initializer = expression()
         }
-        consumeTokenOrThrowError(TokenType.SEMICOLON, "Expect ';' after variable declaration.")
+        consumeNextTokenOrThrowError(TokenType.SEMICOLON, "Expect ';' after variable declaration.")
         return Stmt.Var(name, initializer)
     }
 
     private fun statement(): Stmt {
         val stmt: Stmt
-        if (tokens.getNextTokenIfItHasType(TokenType.BREAK)) {
+        if (tokens.consumeNextTokenIfItHasType(TokenType.BREAK)) {
             stmt = breakStatement()
-        } else if (tokens.getNextTokenIfItHasType(TokenType.FOR)) {
+        } else if (tokens.consumeNextTokenIfItHasType(TokenType.FOR)) {
             breakStack.push(true)
             stmt = forStatement()
             breakStack.pop()
-        } else if (tokens.getNextTokenIfItHasType(TokenType.IF)) {
+        } else if (tokens.consumeNextTokenIfItHasType(TokenType.IF)) {
             stmt = ifStatement()
-        } else if (tokens.getNextTokenIfItHasType(TokenType.PRINT)) {
+        } else if (tokens.consumeNextTokenIfItHasType(TokenType.PRINT)) {
             stmt = printStatement()
-        } else if (tokens.getNextTokenIfItHasType(TokenType.RETURN)) {
+        } else if (tokens.consumeNextTokenIfItHasType(TokenType.RETURN)) {
             stmt = returnStatement()
-        } else if (tokens.getNextTokenIfItHasType(TokenType.WHILE)) {
+        } else if (tokens.consumeNextTokenIfItHasType(TokenType.WHILE)) {
             breakStack.push(true)
             stmt = whileStatement()
             breakStack.pop()
-        } else if (tokens.getNextTokenIfItHasType(TokenType.LEFT_BRACE)) {
+        } else if (tokens.consumeNextTokenIfItHasType(TokenType.LEFT_BRACE)) {
             stmt = Stmt.Block(block())
         } else {
             stmt = expressionStatement()
@@ -135,7 +137,7 @@ class Parser(
 
     private fun breakStatement(): Stmt {
         val breakToken = tokens.getPreviousToken()
-        consumeTokenOrThrowError(TokenType.SEMICOLON, "Expect ';' after 'break'.")
+        consumeNextTokenOrThrowError(TokenType.SEMICOLON, "Expect ';' after 'break'.")
         if (!breakStack.peek()) {
             throw reportErrorAndThrowParseException(breakToken, "'break' is only allowed inside a loop.")
         }
@@ -144,10 +146,10 @@ class Parser(
 
     // A "for loop" in Lox is syntactic sugar. So we desugar it here and convert it to a "while loop".
     private fun forStatement(): Stmt {
-        consumeTokenOrThrowError(TokenType.LEFT_PAREN, "Expect '(' after 'for'.")
-        val initializer: Stmt? = if (tokens.getNextTokenIfItHasType(TokenType.SEMICOLON)) {
+        consumeNextTokenOrThrowError(TokenType.LEFT_PAREN, "Expect '(' after 'for'.")
+        val initializer: Stmt? = if (tokens.consumeNextTokenIfItHasType(TokenType.SEMICOLON)) {
             null
-        } else if (tokens.getNextTokenIfItHasType(TokenType.VAR)) {
+        } else if (tokens.consumeNextTokenIfItHasType(TokenType.VAR)) {
             varDeclaration()
         } else {
             expressionStatement()
@@ -157,13 +159,13 @@ class Parser(
         } else {
             expression()
         }
-        consumeTokenOrThrowError(TokenType.SEMICOLON, "Expect ';' after loop condition.")
+        consumeNextTokenOrThrowError(TokenType.SEMICOLON, "Expect ';' after loop condition.")
         val increment: Expr? = if (tokens.nextTokenHasType(TokenType.RIGHT_PAREN)) {
             null
         } else {
             expression()
         }
-        consumeTokenOrThrowError(TokenType.RIGHT_PAREN, "Expect ')' after for clauses.")
+        consumeNextTokenOrThrowError(TokenType.RIGHT_PAREN, "Expect ')' after for clauses.")
         var body: Stmt = statement()
         if (increment != null) {
             body = Stmt.Block(listOf(body, Stmt.Expression(increment)))
@@ -179,11 +181,11 @@ class Parser(
     }
 
     private fun ifStatement(): Stmt {
-        consumeTokenOrThrowError(TokenType.LEFT_PAREN, "Expect '(' after 'if'.")
+        consumeNextTokenOrThrowError(TokenType.LEFT_PAREN, "Expect '(' after 'if'.")
         val condition = expression()
-        consumeTokenOrThrowError(TokenType.RIGHT_PAREN, "Expect ')' after if condition.")
+        consumeNextTokenOrThrowError(TokenType.RIGHT_PAREN, "Expect ')' after if condition.")
         val thenBranch = statement()
-        val elseBranch: Stmt? = if (tokens.getNextTokenIfItHasType(TokenType.ELSE)) {
+        val elseBranch: Stmt? = if (tokens.consumeNextTokenIfItHasType(TokenType.ELSE)) {
             statement()
         } else {
             null
@@ -193,7 +195,7 @@ class Parser(
 
     private fun printStatement(): Stmt {
         val value = expression()
-        consumeTokenOrThrowError(TokenType.SEMICOLON, "Expect ';' after expression.")
+        consumeNextTokenOrThrowError(TokenType.SEMICOLON, "Expect ';' after expression.")
         return Stmt.Print(value)
     }
 
@@ -204,14 +206,14 @@ class Parser(
         } else {
             expression()
         }
-        consumeTokenOrThrowError(TokenType.SEMICOLON, "Expect ';' after return value.")
+        consumeNextTokenOrThrowError(TokenType.SEMICOLON, "Expect ';' after return value.")
         return Stmt.Return(keyword, value)
     }
 
     private fun whileStatement(): Stmt {
-        consumeTokenOrThrowError(TokenType.LEFT_PAREN, "Expect '(' after 'while'.")
+        consumeNextTokenOrThrowError(TokenType.LEFT_PAREN, "Expect '(' after 'while'.")
         val condition = expression()
-        consumeTokenOrThrowError(TokenType.RIGHT_PAREN, "Expect ')' after condition.")
+        consumeNextTokenOrThrowError(TokenType.RIGHT_PAREN, "Expect ')' after condition.")
         val body = statement()
         return Stmt.While(condition, body)
     }
@@ -224,13 +226,13 @@ class Parser(
                 statements += declaration
             }
         }
-        consumeTokenOrThrowError(TokenType.RIGHT_BRACE, "Expect '}' after block.")
+        consumeNextTokenOrThrowError(TokenType.RIGHT_BRACE, "Expect '}' after block.")
         return statements
     }
 
     private fun expressionStatement(): Stmt {
         val expr = expression()
-        consumeTokenOrThrowError(TokenType.SEMICOLON, "Expect ';' after expression.")
+        consumeNextTokenOrThrowError(TokenType.SEMICOLON, "Expect ';' after expression.")
         return Stmt.Expression(expr)
     }
 
@@ -238,7 +240,7 @@ class Parser(
 
     private fun assignment(): Expr {
         var expr: Expr = or()
-        if (tokens.getNextTokenIfItHasType(TokenType.EQUAL)) {
+        if (tokens.consumeNextTokenIfItHasType(TokenType.EQUAL)) {
             val equals: Token = tokens.getPreviousToken()
             val value: Expr = assignment()
             if (expr is Expr.Variable) {
@@ -259,7 +261,7 @@ class Parser(
 
     private fun or(): Expr {
         var expr: Expr = and()
-        while (tokens.getNextTokenIfItHasType(TokenType.OR)) {
+        while (tokens.consumeNextTokenIfItHasType(TokenType.OR)) {
             val operator: Token = tokens.getPreviousToken()
             val right: Expr = and()
             expr = Logical(expr, operator, right)
@@ -269,7 +271,7 @@ class Parser(
 
     private fun and(): Expr {
         var expr = equality()
-        while (tokens.getNextTokenIfItHasType(TokenType.AND)) {
+        while (tokens.consumeNextTokenIfItHasType(TokenType.AND)) {
             val operator: Token = tokens.getPreviousToken()
             val right = equality()
             expr = Logical(expr, operator, right)
@@ -279,7 +281,7 @@ class Parser(
 
     private fun equality(): Expr {
         var expr: Expr = comparison()
-        while (tokens.getNextTokenIfItHasType(TokenType.BANG_EQUAL, TokenType.EQUAL_EQUAL)) {
+        while (tokens.consumeNextTokenIfItHasType(TokenType.BANG_EQUAL, TokenType.EQUAL_EQUAL)) {
             val operator: Token = tokens.getPreviousToken()
             val right: Expr = comparison()
             expr = Expr.Binary(expr, operator, right)
@@ -289,7 +291,7 @@ class Parser(
 
     private fun comparison(): Expr {
         var expr: Expr = term()
-        while (tokens.getNextTokenIfItHasType(
+        while (tokens.consumeNextTokenIfItHasType(
                 TokenType.GREATER, TokenType.GREATER_EQUAL, TokenType.LESS, TokenType.LESS_EQUAL
             )
         ) {
@@ -302,7 +304,7 @@ class Parser(
 
     private fun term(): Expr {
         var expr: Expr = factor()
-        while (tokens.getNextTokenIfItHasType(TokenType.MINUS, TokenType.PLUS)) {
+        while (tokens.consumeNextTokenIfItHasType(TokenType.MINUS, TokenType.PLUS)) {
             val operator: Token = tokens.getPreviousToken()
             val right: Expr = factor()
             expr = Expr.Binary(expr, operator, right)
@@ -312,7 +314,7 @@ class Parser(
 
     private fun factor(): Expr {
         var expr: Expr = unary()
-        while (tokens.getNextTokenIfItHasType(TokenType.SLASH, TokenType.STAR)) {
+        while (tokens.consumeNextTokenIfItHasType(TokenType.SLASH, TokenType.STAR)) {
             val operator: Token = tokens.getPreviousToken()
             val right: Expr = unary()
             expr = Expr.Binary(expr, operator, right)
@@ -320,7 +322,7 @@ class Parser(
         return expr
     }
 
-    private fun unary(): Expr = if (tokens.getNextTokenIfItHasType(TokenType.BANG, TokenType.MINUS)) {
+    private fun unary(): Expr = if (tokens.consumeNextTokenIfItHasType(TokenType.BANG, TokenType.MINUS)) {
         val operator: Token = tokens.getPreviousToken()
         val right = unary()
         Expr.Unary(operator, right)
@@ -350,60 +352,79 @@ class Parser(
                     errorReporter.error(tokens.peekNextToken(), "Can't have more than 255 arguments.")
                 }
                 arguments += expression()
-            } while (tokens.getNextTokenIfItHasType(TokenType.COMMA))
+            } while (tokens.consumeNextTokenIfItHasType(TokenType.COMMA))
         }
-        val paren: Token = consumeTokenOrThrowError(
+        val paren: Token = consumeNextTokenOrThrowError(
             TokenType.RIGHT_PAREN, "Expect ')' after arguments."
         )
         return Expr.Call(callee, paren, arguments)
     }
 
-    private fun primary(): Expr = if (tokens.getNextTokenIfItHasType(TokenType.FALSE)) {
+    private fun primary(): Expr = if (tokens.consumeNextTokenIfItHasType(TokenType.FALSE)) {
         Expr.Literal(false)
-    } else if (tokens.getNextTokenIfItHasType(TokenType.TRUE)) {
+    } else if (tokens.consumeNextTokenIfItHasType(TokenType.TRUE)) {
         Expr.Literal(true)
-    } else if (tokens.getNextTokenIfItHasType(TokenType.NIL)) {
+    } else if (tokens.consumeNextTokenIfItHasType(TokenType.NIL)) {
         Expr.Literal(null)
-    } else if (tokens.getNextTokenIfItHasType(TokenType.NUMBER, TokenType.STRING)) {
+    } else if (tokens.consumeNextTokenIfItHasType(TokenType.NUMBER, TokenType.STRING)) {
         Expr.Literal(tokens.getPreviousToken().literal)
-    } else if (tokens.getNextTokenIfItHasType(TokenType.IDENTIFIER)) {
+    } else if (tokens.consumeNextTokenIfItHasType(TokenType.IDENTIFIER)) {
         Expr.Variable(tokens.getPreviousToken())
-    } else if (tokens.getNextTokenIfItHasType(TokenType.LEFT_PAREN)) {
+    } else if (tokens.consumeNextTokenIfItHasType(TokenType.LEFT_PAREN)) {
         val expr = expression()
-        consumeTokenOrThrowError(TokenType.RIGHT_PAREN, "Expect ')' after expression.")
+        consumeNextTokenOrThrowError(
+            expectedTokenType = TokenType.RIGHT_PAREN,
+            errorMessage = "Expect ')' after expression."
+        )
         Expr.Grouping(expr)
     } else {
-        throw reportErrorAndThrowParseException(tokens.peekNextToken(), "Expect expression.")
+        throw reportErrorAndThrowParseException(
+            unexpectedToken = tokens.peekNextToken(),
+            errorMessage = "Expect expression."
+        )
     }
 
-    // Checks to see if the next token is of the expected type. If so, it consumes the token.
-    // Otherwise, we report an error.
-    private fun consumeTokenOrThrowError(type: TokenType, errorMessage: String): Token {
-        if (tokens.nextTokenHasType(type)) {
-            return tokens.getNextToken()
+    /**
+     * Checks to see if the next token is of the given type. If so, it consumes the token.
+     * Otherwise, it reports an error.
+     *
+     * In the reference Java implementation this method is called 'consume'.
+     */
+    private fun consumeNextTokenOrThrowError(expectedTokenType: TokenType, errorMessage: String): Token {
+        if (tokens.nextTokenHasType(expectedTokenType)) {
+            return tokens.consumeNextToken()
         } else {
-            throw reportErrorAndThrowParseException(tokens.peekNextToken(), errorMessage)
+            throw reportErrorAndThrowParseException(
+                unexpectedToken = tokens.peekNextToken(),
+                errorMessage = errorMessage
+            )
         }
     }
 
-    private fun reportErrorAndThrowParseException(token: Token, errorMessage: String): ParseError {
-        errorReporter.error(token, errorMessage)
+    /**
+     * Reports an error and then throws a ParseException.
+     *
+     * In the reference Java implementation this method is called 'error'.
+     */
+    private fun reportErrorAndThrowParseException(unexpectedToken: Token, errorMessage: String): ParseError {
+        errorReporter.error(unexpectedToken, errorMessage)
         return ParseError()
     }
 
-    // When the parser encounters an error, this method is called to discard some tokens until
-    // we reach a boundary. We define boundary as the start of a new expression or when we see
-    // a semicolon.
+    /**
+     * This method is called when the parser encounters a syntax error. It discards tokens until we
+     * reach a boundary. We define the boundary as the start of a new expression or when we see a semicolon.
+     */
     private fun synchronize() {
-        tokens.getNextToken()
+        tokens.consumeNextToken()
         while (!tokens.isAtEnd()) {
             if (tokens.getPreviousToken().type == TokenType.SEMICOLON) {
                 return
             }
             when (tokens.peekNextToken().type) {
                 TokenType.CLASS, TokenType.FUN, TokenType.VAR, TokenType.FOR, TokenType.IF, TokenType.WHILE, TokenType.PRINT, TokenType.RETURN -> return
+                else -> tokens.consumeNextToken()
             }
-            tokens.getNextToken()
         }
     }
 
--- src/main/java/ir/fallahpoor/kotlox/interpreter/parser/Tokens.kt
+++ src/main/java/ir/fallahpoor/kotlox/interpreter/parser/Tokens.kt
@@ -7,49 +7,80 @@ class Tokens(private val tokens: List<Token>) {
 
     private var currentTokenIndex = 0
 
-    // match
-    fun getNextTokenIfItHasType(vararg types: TokenType): Boolean {
+    /**
+     * Checks to see if the next unconsumed token has any of the given types. If so, it consumes
+     * the token and returns true. Otherwise, it returns false and does not consume the token.
+     *
+     * In the reference Java implementation this method is called 'match'.
+     */
+    fun consumeNextTokenIfItHasType(vararg types: TokenType): Boolean {
         for (type in types) {
             if (nextTokenHasType(type)) {
-                getNextToken()
+                consumeNextToken()
                 return true
             }
         }
         return false
     }
 
-    // check
-    fun nextTokenHasType(type: TokenType): Boolean =
-        if (isAtEnd()) {
-            false
-        } else {
-            peekNextToken().type == type
-        }
+    /**
+     * Checks if the next unconsumed token has the given type. If so it returns true,
+     * otherwise it returns false.
+     *
+     * In the reference Java implementation this method is called 'check'.
+     */
+    fun nextTokenHasType(type: TokenType): Boolean = if (isAtEnd()) {
+        false
+    } else {
+        peekNextToken().type == type
+    }
 
-    // advance
-    fun getNextToken(): Token {
+    /**
+     * Consumes the next unconsumed token and returns it.
+     *
+     * In the reference Java implementation this method is called 'advance'.
+     */
+    fun consumeNextToken(): Token {
         if (!isAtEnd()) {
             currentTokenIndex++
         }
         return getPreviousToken()
     }
 
-    // Returns the most recently consumed token.
-    // previous
-    fun getPreviousToken(): Token =
-        if (currentTokenIndex >= 1) {
-            tokens[currentTokenIndex - 1]
+    /**
+     * Returns the last consumed token.
+     *
+     * In the reference Java implementation this method is called 'previous'.
+     */
+    fun getPreviousToken(): Token {
+        val previousIndex = currentTokenIndex - 1
+        return if (previousIndex in tokens.indices) {
+            tokens[previousIndex]
+        } else if (previousIndex < 0) {
+            tokens.first()
         } else {
-            tokens[0]
+            tokens.last()
         }
+    }
 
-    // FIXME check currentTokenIndex to be in range
-    // Returns the next token we have yet to consume.
-    // peek
-    fun peekNextToken(): Token = tokens[currentTokenIndex]
+    /**
+     * Returns the next unconsumed token without consuming it.
+     *
+     * In the reference Java implementation this method is called 'peek'.
+     */
+    fun peekNextToken(): Token = if (currentTokenIndex in tokens.indices) {
+        tokens[currentTokenIndex]
+    } else if (currentTokenIndex < 0) {
+        tokens.first()
+    } else {
+        tokens.last()
+    }
 
     // FIXME the very last token of the tokens list should always be a EOF token otherwise
-    //  this method will not work correctly
+    //  this method will not work correctly.
+    /**
+     * Checks if we have run out of token.
+     */
     fun isAtEnd() = peekNextToken().type == TokenType.EOF
 
 }
